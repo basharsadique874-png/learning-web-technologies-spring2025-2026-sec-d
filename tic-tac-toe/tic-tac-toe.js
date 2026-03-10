3b9ae4b1a567cb5eecdedd1ee41756f714127f93
unknown <basharsadique874@gmail.com>
2026-03-10 17:58:09 +0600

    done

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -20,3 +20,65 @@ let winPatterns = [
 cells.forEach(cell => {
     cell.addEventListener("click", cellClick);
 });
+
+resetBtn.addEventListener("click", resetGame);
+
+function cellClick(){
+    let index = this.getAttribute("data-index");
+
+    if(board[index] != "" || !gameRunning){
+        return;
+    }
+
+    board[index] = currentPlayer;
+    this.textContent = currentPlayer;
+
+    checkWinner();
+
+    if(gameRunning){
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        statusText.textContent = "Current Player: " + currentPlayer;
+    }
+}
+
+function checkWinner(){
+
+    for(let i=0;i<winPatterns.length;i++){
+        let a = winPatterns[i][0];
+        let b = winPatterns[i][1];
+        let c = winPatterns[i][2];
+
+        if(board[a] == "" || board[b] == "" || board[c] == ""){
+            continue;
+        }
+
+        if(board[a] == board[b] && board[b] == board[c]){
+            gameRunning = false;
+            statusText.textContent = "Winner: " + currentPlayer;
+
+            cells[a].classList.add("win");
+            cells[b].classList.add("win");
+            cells[c].classList.add("win");
+
+            return;
+        }
+    }
+
+    if(!board.includes("")){
+        statusText.textContent = "It's a Draw!";
+        gameRunning = false;
+    }
+}
+
+function resetGame(){
+    board = ["","","","","","","","",""];
+    currentPlayer = "X";
+    gameRunning = true;
+
+    statusText.textContent = "Current Player: X";
+
+    cells.forEach(cell => {
+        cell.textContent = "";
+        cell.classList.remove("win");
+    });
+}
