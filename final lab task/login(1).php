a227aba72e8d12dacb3aca10b54c0aab9b0ec546
unknown <basharsadique874@gmail.com>
2026-04-21 17:50:36 +0600

    login page done

---
+++ final lab task/login(1).php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h2>LOGIN</h2>
+
+<form action="loginval.php" method="post">
+    User Name: <input type="text" name="username"><br><br>
+
+    Password: <input type="password" name="password"><br><br>
+
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
